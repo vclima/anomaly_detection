8486d9d864ca924e50dc520594dc59a22340d217
Victor Lima <vlima@riseup.net>
2022-07-11 17:00:11 -0300

    Removed regex

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from pathlib import Path
 from os.path import getctime
 from os import unlink
 from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler,RegexMatchingEventHandler
+from watchdog.events import FileSystemEventHandler
 
 folderPath='ftp'
 
-class NewfileHandler(RegexMatchingEventHandler):
+class NewfileHandler(FileSystemEventHandler):
     file_list=[]
 
     def on_created(self, event): # when file is created
@@ -30,7 +30,7 @@ class NewfileHandler(RegexMatchingEventHandler):
             file_list=sorted(Path(folderPath).iterdir(),key=getctime)
 
 observer = Observer()
-event_handler = NewfileHandler(ignore_regexes=['.*_TEMP*'],ignore_directories=True) # create event handler
+event_handler = NewfileHandler(ignore_directories=True) # create event handler
 # set observer to use created handler in directory
 observer.schedule(event_handler, path=folderPath)
 observer.start()
