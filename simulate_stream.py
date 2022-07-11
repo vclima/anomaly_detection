525627130ff54d1d88bcc1c9e1ea4fd3c99c6f7e
Victor Lima <vlima@riseup.net>
2022-07-11 17:04:23 -0300

    simulate stream

---
+++ simulate_stream.py
@@ -0,0 +1,33 @@
+import os
+import shutil
+import time
+
+temp_string='temp_'
+
+def main(src='cams0/i3t',dest='ftp',delta_t=5):
+    print('simulating input stream...')
+    print('copying files from',src,' to ',dest)
+
+    # wipe destination directory
+    for filename in os.listdir(dest):
+        os.remove(os.path.join(dest,filename))
+        
+    for k,filename in enumerate(os.listdir(src)):
+        # create temp filename
+        temp_filename=temp_string+filename
+        print(temp_filename)
+        print(filename)
+        
+        # copy one file
+        shutil.copy(os.path.join(src,filename),os.path.join(dest,temp_filename))
+        print('copied file number ',k)
+           
+        # rename copied filed
+        os.rename(os.path.join(dest,temp_filename),os.path.join(dest,filename))
+        
+        # sleep delta_t seconds
+        time.sleep(delta_t)
+     
+     
+if __name__ == "__main__":
+    main()
