9f289382e717f0550afa0b0a3712106e1eff4510
Victor Lima <vlima@riseup.net>
2022-07-12 10:51:03 -0300

    changed monitor.py

--- monitor.py
+++ monitor.py
@@ -3,23 +3,35 @@ from time import sleep
 from pathlib import Path
 from os.path import getctime
 from os import unlink
+#from decompLib import Decomp
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
-folderPath='ftp'
+camPath='ftp'
+figshape=[0,0]
+
+
+
+trainPath='train/'+camPath
+debugPath='debug/'+camPath
+dicioPath='dicio/'+camPath
+
+
 
 class NewfileHandler(FileSystemEventHandler):
     file_list=[]
 
     def on_created(self, event): # when file is created
         # do something, eg. call your function to process the image
-        print("Got created event for file"+str(event.src_path))
+        print("Got created event for file "+str(event.src_path))
+        fileName=event.src_path.split('.')
+        fileName=fileName+'.bin'
         try:
-            file_list=sorted(Path(folderPath).iterdir(),key=getctime)
+            file_list=sorted(Path(camPath).iterdir(),key=getctime)
             while len(file_list)>=30:
                 unlink(file_list[0])
                 print('Deleted '+str(file_list[0]))
-                file_list=sorted(Path(folderPath).iterdir(),key=getctime)
+                file_list=sorted(Path(camPath).iterdir(),key=getctime)
         except:
             pass
 '''   
@@ -36,9 +48,11 @@ class NewfileHandler(FileSystemEventHandler):
 observer = Observer()
 event_handler = NewfileHandler() # create event handler
 # set observer to use created handler in directory
-observer.schedule(event_handler, path=folderPath)
-observer.start()
+observer.schedule(event_handler, path=camPath)
+#instanciar um objeto da classe Decomp
 
+observer.start()
+#process=Decomp(figshape,dicioPath)
 # sleep until keyboard interrupt, then stop + rejoin the observer
 try:
     while True:
