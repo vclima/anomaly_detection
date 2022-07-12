f5de01c49138ee3ac10e5a31b37268f78830ebd9
Victor Lima <vlima@riseup.net>
2022-07-12 14:21:15 -0300

    binOpen works with RGB now

--- simulate_stream.py
+++ simulate_stream.py
@@ -4,7 +4,7 @@ import time
 
 temp_string='temp_'
 
-def main(src='cams1/i3t',dest='i3t',delta_t=5):
+def main(src='cams2/i3t',dest='i3t',delta_t=5):
     print('simulating input stream...')
     print('copying files from',src,' to ',dest)
 
--- utils.py
+++ utils.py
@@ -1,15 +1,34 @@
-from struct import iter_unpack
+from struct import iter_unpack,unpack
 import numpy as np
+import cv2
 
 
 def binOpen(fileName):
     filePointer=open(fileName,'rb')
+    
+    contentBin=filePointer.read(2)
+    content=unpack('H',contentBin)
+    img_type=content 
+    
     contentBin=filePointer.read(4)
     content=list(iter_unpack('H',contentBin))
     fig_shape=(content[1][0],content[0][0])
 
-    contentBin=filePointer.read(2*fig_shape[0]*fig_shape[1])
-    content=list(iter_unpack('H',contentBin))
-    image=np.array(content).squeeze()
-    image=np.reshape(image,fig_shape)
+    if img_type:
+        contentBin=filePointer.read(2*fig_shape[0]*fig_shape[1])
+        content=list(iter_unpack('H',contentBin))
+        image=np.array(content).squeeze()
+        image=np.reshape(image,fig_shape)
+        image = normalize(image)
+    else: 
+        contentBin=filePointer.read(2*fig_shape[0]*fig_shape[1])
+        content=list(iter_unpack('H',contentBin))
+        image=np.array(content).squeeze()
+        image=np.reshape(image,fig_shape)
+        image = cv2.cvtColor(image, cv2.COLOR_BayerGR2GRAY)
+        image = normalize(image)
     return image
+
+def normalize(img):
+    out=cv2.normalize(img,None,1,0,cv2.NORM_MINMAX,cv2.CV_32F)
+    return out
